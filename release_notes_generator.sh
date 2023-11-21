bb6ef66d03ffbd7c374e5634a70dfe5e8b91b30b
husam-e <2340614+husam-e@users.noreply.github.com>
2023-11-21 13:52:49 -0500

    update usage and add scopes

--- release_notes_generator.sh
+++ release_notes_generator.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 read -r -d '' USAGE << EOM
-Usage: release_notes_helper.sh [OPTIONS] <START_VERSION> [END_VERSION]
+Usage: release_notes_helper.sh [-j JIRAKEY] [-s SCOPES] <START_VERSION> [END_VERSION]
 
-    example 1 (from a release tag to main, for an upcoming tag):     ./release_notes_helper.sh v1.8.0 origin/main
-    example 2 (from a certain start revision until the latest HEAD): ./release_notes_helper.sh v1.8.0
-    example 3 (between two commits/tags):                            ./release_notes_helper.sh v1.8.0 v1.9.0
+    example 1 (from a release tag to main, for an upcoming tag):      ./release_notes_helper.sh v1.1.0 origin/main
+    example 2 (from a certain start revision until the latest HEAD):  ./release_notes_helper.sh v1.1.0
+    example 3 (between two commits/tags):                             ./release_notes_helper.sh v1.1.0 v1.2.0
+    example 4 (from a release tag to main, with JIRA key):            ./release_notes_helper.sh -j MYPROJ v1.1.0 origin/main
+    example 5 (from a release tag to main, with JIRA key and scopes): ./release_notes_helper.sh -j MYPROJ -s "(ui)\|(backend)" v1.1.0 origin/main
 
+"-j": optional, the JIRA Project Key e.g. "MYPROJ".
+"-s": optinal, the scopes to filter commits for - anything that regular `grep` can understand and find in the commit message. Default: ".*"
 START_VERSION: required
 END_VERSION: optional, default: HEAD
 EOM
 
-while getopts ":fh" opt; do
+while getopts ":j:s:e:fh" opt; do
     case ${opt} in
+        j) JIRAKEY="${OPTARG}"
+            ;;
+        s) SCOPES="${OPTARG}"
+            ;;
+        h) echo "${USAGE}" && exit 0
+            ;;
         \? ) echo "${USAGE}" && exit -2
             ;;
     esac
@@ -29,10 +39,14 @@ if [ -z "${END_VERSION}" ]; then
   END_VERSION="HEAD"
 fi
 
-if [ -z "${JIRAPROJECT}" ]; then
+if [ -z "${JIRAKEY}" ]; then
   ISSUEPREFIX="#" # use default Github/GitLab issue prefix if no JIRA project specified
 else
-  ISSUEPREFIX="${JIRAPROJECT}-"
+  ISSUEPREFIX="${JIRAKEY}-"
+fi
+
+if [ -z "${SCOPES}" ]; then
+  SCOPES=".*" # default to everything (a no-op)
 fi
 
 echo "Running a git fetch to update local repositories..."
@@ -43,6 +57,7 @@ echo "Generating release notes from ${START_VERSION} to ${END_VERSION}"
 echo "Copy the lines below into the release notes."
 echo "-----"
 git log ${START_VERSION}...${END_VERSION} --pretty=format:"- [(%h)](${CI_PROJECT_URL}/commit/%H) %s" --reverse |
+    grep "${SCOPES}" |
     sort -k 3,3 |
     sed -e $'s/*/\\n  */g' |
     sed -e $'s/feat:/:trophy: *feat:*/g' |
@@ -79,5 +94,5 @@ issues=($(git log ${START_VERSION}...${END_VERSION} --pretty=format:'%s' --rever
 unique=($(echo "${issues[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 echo "Commit range includes ${#unique[@]} Issues"
 list=$(IFS=, ; echo "${unique[*]}")
-# If JIRAPROJECT is set, output JIRA query
-[[ ! -z "$JIRAPROJECT" ]] && echo "project=${JIRAPROJECT} and id in (${list})"
+# If JIRAKEY is set, output JIRA query
+[[ ! -z "$JIRAKEY" ]] && echo "project=${JIRAKEY} and id in (${list})"
